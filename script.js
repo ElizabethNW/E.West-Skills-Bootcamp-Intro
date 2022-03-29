777425e5f25d5cc1fa5ee6900ec27b9989e837ff
ElizabethNW <nnye_liz@yahoo.com>
2022-03-29 12:01:53 +0000

    contact form

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-  
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}
+(function () {
+    emailjs.init("user_d1oKOZEWf4eIedK3zMVgl");
+})();
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }    
+function sendMail(contactForm) {
+    emailjs.send("service_78z8ikf","template_k1qm4yp", {
+            type_of_request: contactForm.subject.value,
+            from_name: contactForm.name.value,
+            from_email: contactForm.emailaddress.value,
+            project_request: contactForm.projectsummary.value,
+        })
+        .then(
+            function (response) {
+                alert("The email has been sent successfully.");
+                $("#mail-form").get(0).reset();
+            },
+            function (error) {
+                alert("Try again.");
+            }
+        );
+    return false; 
 }
-
-toggleSwitch.addEventListener('change', switchTheme, false);
